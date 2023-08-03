38c5889e146f55f4d6a2a857be9eb5dca809d6bb
Leila Ibañez <133930984+Leila-ibz@users.noreply.github.com>
2023-08-02 23:16:47 -0300

    Conflicto arreglado 1

--- blog/blog/configuraciones/local.py
+++ blog/blog/configuraciones/local.py
@@ -1,20 +1,21 @@
 from .settings import *
-#DB Leila SQL
-# DATABASES = { 
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'blogdb',
-#         'USER': 'root',
-#         'PASSWORD': 'root',
-#         'HOST': 'localhost',  
-#         'PORT': '3306',        
-#     }
-# }
+# DB Leila SQL
 
-#DB Neko Local
-DATABASES = {
+DATABASES = { 
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Ruta a tu archivo de base de datos SQLite.
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogdb',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  
+        'PORT': '3306',        
     }
 }
+
+# #DB Neko Local
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',  # Ruta a tu archivo de base de datos SQLite.
+#     }
+# } 
